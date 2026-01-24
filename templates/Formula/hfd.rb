e3112fb9634285ba75db4616312ac01148baf586
mzwing <mzwing@mzwing.eu.org>
2026-01-25 00:21:58 +0800

    fix: typo

--- templates/Formula/hfd.rb
+++ templates/Formula/hfd.rb
@@ -1,4 +1,4 @@
-class StableDiffusionCpp < Formula
+class Hfd < Formula
   desc "🤗Huggingface Model Downloader"
   homepage "https://gist.github.com/padeoe/697678ab8e528b85a2a7bddafea1fa4f"
   head "https://gist.github.com/697678ab8e528b85a2a7bddafea1fa4f.git"
