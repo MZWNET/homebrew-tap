ac660b3cbc4f87d248959c77e047410ce0bb6741
mzwing <mzwing@mzwing.eu.org>
2026-03-20 11:24:12 +0800

    fix(astrbot-desktop): local sign the app to bypass gatekeeper

--- Casks/astrbot-desktop.rb
+++ Casks/astrbot-desktop.rb
@@ -9,4 +9,9 @@ cask "astrbot-desktop" do
   homepage "https://github.com/AstrBotDevs/AstrBot-desktop"
 
   app "AstrBot.app"
+
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-cr", "/Applications/AstrBot.app"]
+    system_command "/usr/bin/codesign", args: ["-fs", "-", "/Applications/AstrBot.app"]
+  end
 end
