fbae2e36b693045aa3a30bcfbb8eba5dac6f3327
mzwing <mzwing@mzwing.eu.org>
2026-06-06 17:18:01 +0800

    fix(hydroxide): license

--- Formula/hydroxide.rb
+++ Formula/hydroxide.rb
@@ -1,10 +1,10 @@
 class Hydroxide < Formula
-  desc "A third-party, open-source ProtonMail CardDAV, IMAP and SMTP bridge"
+  desc "Third-party, open-source ProtonMail CardDAV, IMAP and SMTP bridge"
   homepage "https://github.com/emersion/hydroxide"
   url "https://github.com/MZWNET/actions/releases/download/hydroxide-v0.2.32/hydroxide-v0.2.32-macos-arm64.zip"
   version "0.2.32"
   sha256 "a4788086bcfc0fc5712faef7c3c7f33d8bb49516c9017e522f05a96287472dc2"
-  license "Apache-2.0"
+  license "MIT"
   head "https://github.com/emersion/hydroxide.git", branch: "master"
 
   def install
